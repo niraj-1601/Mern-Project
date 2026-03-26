bb37554b4c72dcb7bc3b26b73ae99318f89b434e
niraj-1601 <nirajpatil010607@gmail.com>
2026-03-26 17:28:57 +0530

    import sample videos

--- youtube-clone/frontend/src/data/sampleVideos.js
+++ youtube-clone/frontend/src/data/sampleVideos.js
@@ -20,8 +20,9 @@ export const videos = [
   {
 
     videoId: "video03",
-    
 
+
+    
     title: "CSS Flexbox Tutorial",
     thumbnailUrl: "https://i.ytimg.com/vi/JJSoEo8JSnc/hqdefault.jpg",
     description: "Learn Flexbox fast.",

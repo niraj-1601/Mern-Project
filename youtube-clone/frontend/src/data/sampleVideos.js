3bdcd16fa7a07583a7b5e28f1a528e4a9a12176c
niraj-1601 <nirajpatil010607@gmail.com>
2026-03-26 17:32:15 +0530

    create channel model

--- youtube-clone/frontend/src/data/sampleVideos.js
+++ youtube-clone/frontend/src/data/sampleVideos.js
@@ -21,14 +21,16 @@ export const videos = [
 
     videoId: "video03",
 
-    
+
 
 
     title: "CSS Flexbox Tutorial",
     thumbnailUrl: "https://i.ytimg.com/vi/JJSoEo8JSnc/hqdefault.jpg",
     description: "Learn Flexbox fast.",
     channelName: "CSS Master",
+
     views: 8345,
 
+    
   },
 ];
